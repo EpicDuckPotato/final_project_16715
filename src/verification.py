e1a1957c96e72553206301692eed38c1681f3f47
Anoop Bhat <noopy.g.bhat@gmail.com>
2022-11-04 14:32:19 -0400

    Some cleanup, i.e. we don't have to pass V and dV directly, more freedom. Also added capability for multiple lambda polynomials

--- src/verification.py
+++ src/verification.py
@@ -10,28 +10,39 @@ import numpy as np
 np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(suppress=True)
 
-# check_sos: checks if a polynomial is SOS
+# check_sos: finds a set of Lagrange multiplier polynomials [la_1, la_2, ..., la_n]
+# to make the total polynomial L = poly_0 + la_1*poly_1 + ... la_n*poly_n SOS
 # ARGUMENTS
-# V, dV: Lyapunov and its derivative, object of class pydrake.symbolic.Polynomial
-# rho: ROA to check
+# poly0, object of class pydrake.symbolic.Polynomial
 # w: a numpy array, where each element of the array is a pydrake.symbolic.Variable.
-# deg_lam: degree of lamda(w)
-# eps: epsilon
-# RETURN: True/False indicating whether the polynomial is SOS
-def check_sos(V, dV, rho, w, deg_lam=2, eps=0.001, verbose=False):
-  # Build basis of lambda(x), start from bias value 1
-  basis_la = [Expression(1)] 
-  for d in range(1, deg_lam//2+1):
-    combs = combinations_with_replacement(w, r=d) # Unordered
-    for p in combs:
-      basis_la.append(np.prod(p))
-  
-  num_basis_la = len(basis_la)
-  Q_la = MakeMatrixContinuousVariable(num_basis_la, num_basis_la,'Q_la')
-  la = Polynomial(basis_la@Q_la@basis_la, w)  # without w, all variables in m are considered as indeterminates
+# constraint_poly: list of constraint polynomials [poly_1, ... poly_n]
+# la_degrees: list of degrees for Lagrange multiplier polynomials [deg_la_1, ... deg_la_n]
+# la_sos: list indicating whether this multiplier should be SOS. If the constraint
+# is an inequality constraint, it should be SOS. Otherwise, it shouldn't be.
+# All polynomials should be functions of w
+# RETURN: True/False indicating whether we could find a set of
+# Lagrange multiplier polynomials to make the total polynomial L SOS
+def check_sos(poly0, w, constraint_poly=[], la_degrees=[], la_sos=[]):
+  # TODO: this only handles SOS multipliers for now, so we couldn't do equality constraints
+  multipliers = [] # List of multiplier polynomials (lambdas)
+  multiplier_Qs = [] # Matrix for each lambda
+  num_multiplier_basis = [] # Basis size for each lambda
+  for i, (deg_la, poly) in enumerate(zip(la_degrees, constraint_poly)):
+    # Build basis of each Lagrange multiplier, starting from bias value 1
+    basis_la = [Expression(1)] 
+    for d in range(1, deg_la//2+1):
+      combs = combinations_with_replacement(w, r=d) # Unordered
+      for p in combs:
+        basis_la.append(np.prod(p))
+    
+    num_basis_la = len(basis_la)
+    num_multiplier_basis.append(num_basis_la)
+    Q_la = MakeMatrixContinuousVariable(num_basis_la, num_basis_la,'Q_la' + str(i))
+    multiplier_Qs.append(Q_la)
+    multipliers.append(Polynomial(basis_la@Q_la@basis_la, w))
 
-  # Build basis of Lagrangian-ish, start from bias value 1
-  deg_L = np.max([dV.TotalDegree(), deg_lam+V.TotalDegree()])
+  # Build basis of L, starting from bias value 1
+  deg_L = np.max([poly0.TotalDegree()] + [deg_la + poly.TotalDegree() for deg_la, poly in zip(la_degrees, constraint_poly)])
   if deg_L%2 == 1:
     # If the polynomial is odd, it can't be SOS, so we automatically return False
     return False  
@@ -44,8 +55,7 @@ def check_sos(V, dV, rho, w, deg_lam=2, eps=0.001, verbose=False):
 
   num_basis = len(basis)
   Q_L = MakeMatrixContinuousVariable(num_basis, num_basis, 'Q_L')
-  w2 = Polynomial(w@w)
-  L = -dV - eps*w2 - la*(rho - V)
+  L = poly0 - sum([la*poly for la, poly in zip(multipliers, constraint_poly)])
   wQw = Polynomial(basis@Q_L@basis, w)
 
   # Dicts where keys are monomials, values are coefficients
@@ -53,40 +63,47 @@ def check_sos(V, dV, rho, w, deg_lam=2, eps=0.001, verbose=False):
   mon2coef_wQw_dict = wQw.monomial_to_coefficient_map()   
 
   with Model("sdo1") as M:
-    Q_L1= M.variable(Domain.inPSDCone(num_basis))
-    Q_la1= M.variable(Domain.inPSDCone(num_basis_la))
+    # PSD matrix variable for LHS
+    Q_L_mosek = M.variable(Domain.inPSDCone(num_basis))
 
-    # Now we set up constraints such that the coefficients of basis_times_Q equal the coefficients of input_poly
+    # PSD matrix variables for RHS lambdas
+    multiplier_Qs_mosek = []
+
+    for num_basis_la in num_multiplier_basis:
+      multiplier_Qs_mosek.append(M.variable(Domain.inPSDCone(num_basis_la)))
+
+    # Now we set up constraints such that the coefficients of w@Q@w equal the coefficients of L
     cidx = 0
     for monom, coef in mon2coef_wQw_dict.items():
-      # coeff is what multiplies the monomial. It's a linear combination of elements of Q, e.g.
+      # coeff is what multiplies the monomial in w@Q@w. It's a linear combination of elements of Q, e.g.
       # a00*Q[0, 0] + a01*Q[1, 1] + ...
       # Here, we get the coefficients a00, a01 to set up the LHS of our constraint.
-      L_QL = np.reshape([coef.Differentiate(q).Evaluate() for q in Q_L.flatten()], Q_L.shape)  
+      C_Q_L = np.reshape([coef.Differentiate(q).Evaluate() for q in Q_L.flatten()], Q_L.shape)  
 
       # Now we get the RHS, i.e. const + a00*Qla[0, 0] + a01*Qla[1, 1] + ... = RHS 
+      C_multipliers = []
       const = 0.0
       if monom in mon2coef_L_dict:
-        # check coef and coef_L, that's what we need to equalize
         coef_L = mon2coef_L_dict[monom]
-        mon2coef_this_dict = Polynomial(coef_L, Variables(Q_la.flatten())).monomial_to_coefficient_map()
-        # get the constant before div
+        for Q_la in multiplier_Qs:
+          C_Q_la = np.reshape([coef_L.Differentiate(q).Evaluate() for q in Q_la.flatten()], Q_la.shape)  
+          C_multipliers.append(C_Q_la)
+
+        mon2coef_this_dict = Polynomial(coef_L).monomial_to_coefficient_map()
         if Monomial() in mon2coef_this_dict:
           const = mon2coef_this_dict[Monomial()]
           const = const.Evaluate()
 
-        L_Qla= np.reshape([coef_L.Differentiate(q).Evaluate() for q in Q_la.flatten()], Q_la.shape)  
-      else:
-        # If the monomial doesn't have any coefficients in input_poly, that implies the coefficient is zero
-        L_Qla = 0.0
-      
-
       # Constraints: equalize the coefficients
-      LHS = Expr.dot(Matrix.dense(L_QL), Q_L1)
-      RHS = Expr.add(Expr.dot(Matrix.dense(L_Qla), Q_la1), const)
-      M.constraint(Expr.sub(LHS, RHS), Domain.equalsTo(0)) # dot returns scalar
+      LHS = Expr.dot(Matrix.dense(C_Q_L), Q_L_mosek)
+      RHS = Expr.constTerm(const)
+      for C_Q_la, Q_la_mosek in zip(C_multipliers, multiplier_Qs_mosek):
+        RHS = Expr.add(RHS, Expr.dot(Matrix.dense(C_Q_la), Q_la_mosek))
+      M.constraint(Expr.sub(LHS, RHS), Domain.equalsTo(0))
+
       # TODO: optimize code
       # do not have to evaluate full matrix because Qmosek is symmetric
+
       cidx += 1
 
     M.solve()
@@ -94,25 +111,6 @@ def check_sos(V, dV, rho, w, deg_lam=2, eps=0.001, verbose=False):
     status = M.getPrimalSolutionStatus()
 
     if status == SolutionStatus.Optimal:
-      # if verbose:
-        # print('The input polynomial was the following:')
-        # print(input_poly)
-        # Qsoln = Qmosek.level().reshape(Q.shape)
-        # print('The polynomial is SOS, with the following PSD matrix:')
-        # print(Qsoln)
-        # print('This corresponds to the following SOS decomposition')
-        # L = np.linalg.cholesky(Qsoln)
-        # polys = L.transpose()@basis
-        # for p, poly in enumerate(polys):
-        #   print_str = '(' + str(poly) + ')^2'
-        #   if p != len(polys) - 1:
-        #     print_str += ' + '
-        #   print(print_str)
-        # print('The resulting polynomial is the following:')
-        # output_poly = Polynomial(basis@Qsoln@basis).RemoveTermsWithSmallCoefficients(1e-7)
-        # print(output_poly)
-        # print()
-
       return True
     else:
       return False
--- test/s_proc_test.py
+++ test/s_proc_test.py
@@ -20,7 +20,7 @@ def verify(gen_V, gen_Vdot, size_w, deg_la, rho, eps):
   w = MakeVectorContinuousVariable(size_w, 'w')
   V = gen_V(w)
   Vdot = gen_Vdot(w)
-  return check_sos(V, Vdot, rho, w, deg_la, eps)
+  return check_sos(-Vdot - eps*Polynomial(w@w), w, [rho - V], [deg_la], [True])
 
 np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(suppress=True)
@@ -46,3 +46,15 @@ for rho in np.linspace(0.1, 3, 20):
 
 def gen_V(w):
   return Polynomial(1.6131259297527509*w[1]**2 + 4.8284271247461827*w[0] * w[1] + 4.6955181300451407*w[0]**2)
+
+def gen_Vdot(w):
+  return Polynomial(-3.9510190129866094e-16*w[1] + -3.6021752652406729*w[1]**2 + -5.9131178232366702e-16*w[0] + -7.7888609948495748*w[0] * w[1] + -6.8284271247461623*w[0]**2 + 1.9755095064933047e-16*w[0]**2 * w[1] + 2.9565589116183351e-16*w[0]**3 + -0.53770864325091694*w[0]**3 * w[1] + -0.80473785412436372*w[0]**4)
+
+size_w = 2
+deg_la = 2
+eps = 0.001
+print('Testing second Lyapunov function')
+for rho in np.linspace(0.05, 0.15, 20):
+  is_sos_drake = verify_drake(gen_V, gen_Vdot, size_w, deg_la, rho, eps)
+  is_sos = verify(gen_V, gen_Vdot, size_w, deg_la, rho, eps)
+  print(f'rho = {rho:.3f} Our solution: {is_sos:1}, Drake solution: {is_sos_drake:1}')
